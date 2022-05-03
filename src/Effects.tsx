6a4dfc6017aab7ddfff0240ed94da551e5db2017
Egor <egor18mochalov@yandex.ru>
2022-05-03 21:49:50 +0300

    react-render

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,26 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useState, useEffect, useCallback } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [Id, setId] = useState(props.sourceId);
+    const [Mes, setMes] = useState(-1);
+    const [save, setSave] = useState(function () {
+        return function (mes: number) {
+            return setMes(mes);
+        };
+    });
+
+    useEffect(() => {
+        subscribe(props.sourceId, save);
+        setId(props.sourceId);
+        setMes(-1);
+        return () => {
+            unsubscribe(props.sourceId, save);
+        };
+    }, [props.sourceId]);
+    return (
+        <div>
+            {Id}: {Mes}
+        </div>
+    );
 }
